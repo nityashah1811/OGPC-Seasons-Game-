295e415242cc18d54b165a0a7e9c2b7b87d60711
tobemuoto <482114@bsd48.org>
2024-01-31 19:00:30 -0800

    I added a sound effect when you eat the apple

--- My project (5)/Assets/Scripts/PlayerEatsApple.cs
+++ My project (5)/Assets/Scripts/PlayerEatsApple.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerEatsApple : MonoBehaviour
 {
+    AudioSource crunchSound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        crunchSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,7 @@ public class PlayerEatsApple : MonoBehaviour
        if (collision.gameObject.tag == "Apple")
         {
             Destroy(collision.gameObject);
+            crunchSound.Play();
         }
     }
 }
