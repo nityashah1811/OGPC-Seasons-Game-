b6b17ca25a5d956f32fd69cf346d4ff5a7566a4c
nimninja <nimayshah1118@gmail.com>
2024-02-01 09:56:03 -0800

    Made some small changes. Backpack works and apple doesnt

--- OGPC/Assets/Scripts/PickUpBackpack.cs
+++ OGPC/Assets/Scripts/PickUpBackpack.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pickedup : MonoBehaviour
+public class PickUpBackpack : MonoBehaviour
 {
     // Start is called before the first frame update
 
