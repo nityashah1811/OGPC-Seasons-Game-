1cf8895ec2ac96026af51171ae410ef163062e86
nimninja <nimayshah1118@gmail.com>
2024-02-05 14:14:41 -0800

    Added crosshair and limited head movement down
    character cannot see body now

--- OGPC/Assets/Scripts/camera.cs
+++ OGPC/Assets/Scripts/camera.cs
@@ -24,7 +24,7 @@ public class camerascript : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 65f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
