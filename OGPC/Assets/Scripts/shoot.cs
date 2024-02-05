398f679a55e7555cbd00d86176b584708d7ddf80
hestonbluchen <chinatigergod@gmail.com>
2024-02-04 22:26:14 -0800

    Fireball + Hand Imported, can shoot fireball with N

---
+++ OGPC/Assets/Scripts/shoot.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+        }
+
+        // Debug line to visualize the forward direction
+        Debug.DrawRay(bulletSpawnPoint.position, bulletSpawnPoint.forward * 10, Color.red, 1.0f);
+    }
+
+}
