d907deb17d806bf7b30b211d79b978bc2f5c0afc
nityashah1811 <nityashah1811@gmail.com>
2024-03-20 11:09:46 -0700

    arrow works points to apple

--- OGPC/Assets/TargetIndicator.cs
+++ OGPC/Assets/TargetIndicator.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class TargetIndicator : MonoBehaviour
 {
-
     public Transform target;
     public float rotationSpeed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public float additionalRotation = 90f; // Additional rotation in degrees
 
     // Update is called once per frame
     void Update()
     {
+        if (target == null)
+            return;
 
+        Vector3 targetDirection = target.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position),
-            rotationSpeed * Time.deltaTime);
+        // Add additional rotation
+        targetRotation *= Quaternion.Euler(0, additionalRotation, 0);
 
+        // Smoothly rotate towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
